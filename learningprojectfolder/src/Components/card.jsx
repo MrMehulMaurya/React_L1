5d28f8517bbb35a3bb4d7bef0b42ad6bc044bd34
Mehul Maurya <mrmehulmauryaofficial@gmail.om>
2026-02-08 19:38:54 +0530

    second commit

---
+++ learningprojectfolder/src/Components/card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (<>
+    <div className="parent">
+        <div className="card">
+
+            <h1>Mehul Maurya</h1>
+            <h2>Lorem ipsum dolor sit amet.</h2>
+            <button>Visit</button>
+        </div>
+        </div></>
+  )
+}
+
+export default Card;
